29975b8e065206550a30574e0e49e621ab2b1189
520xyxyzq <137763257@qq.com>
2024-04-23 15:17:50 -0400

    add efficient sam

---
+++ nerfstudio/utils/effsam_utils.py
@@ -0,0 +1,63 @@
+import torch
+
+from efficient_sam.build_efficient_sam import (
+    build_efficient_sam_vits, build_efficient_sam_vitt
+)
+from nerfstudio.utils.debug_utils import debug_bbox_prompts
+from tqdm import tqdm
+
+# Load EfficientSAM model
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model_type = "vit_t"
+if model_type == "vit_s":
+    effsam = build_efficient_sam_vits()
+elif model_type == "vit_t":
+    effsam = build_efficient_sam_vitt()
+else:
+    raise ValueError("Invalid model type")
+effsam.eval()
+effsam.to(device)
+
+
+def effsam_predict(rgbs, bboxes):
+    """
+    Query SAM model with bboxes prompts
+
+    Args:
+        rgbs: (N, 3, H, W) RGB images
+        bboxes (N, 4): Bbox prompts (xyxy)
+
+    Returns:
+        masks (N, 1, H, W): Image masks
+        scores (N-list): Confidence scores
+    """
+    if rgbs.shape[1] == 1:
+        rgbs = rgbs.repeat(1, 3, 1, 1)
+    elif rgbs.shape[1] == 3:
+        pass
+    else:
+        raise ValueError("RGB images are of shape (N, 3, H, W)")
+    assert bboxes.shape[-1] == 4, \
+        "bbox prompts are of shape (N, 4)"
+    assert bboxes.shape[0] == rgbs.shape[0], \
+        "Image bbox batch mismatch"
+    # Uncomment to debug
+    # debug_bbox_prompts(rgbs, bboxes, "/home/ziqi/Desktop/test/")
+    bbox_pts = bboxes.reshape(bboxes.shape[0], 1, -1, 2)
+    # Make labels for bbox points: 2 for top-left, 3 for bottom-right
+    pts_label = torch.tensor([2, 3]).to(bbox_pts.device).reshape(1, 1, -1)
+    masks, scores = [], []
+    for rgb, bbox_pt in tqdm(zip(rgbs, bbox_pts), desc="EffSAM"):
+        logits, iou = effsam(rgb[None, ...], bbox_pt[None, ...], pts_label)
+        sorted_ids = torch.argsort(iou, dim=-1, descending=True)
+        iou = torch.take_along_dim(iou, sorted_ids, dim=2)
+        logits = torch.take_along_dim(
+            logits, sorted_ids[..., None, None], dim=2
+        )
+        mask = torch.ge(logits[0, 0, 0, :, :], 0)
+        masks.append(mask)
+        scores.append(iou[0, 0, 0].item())
+    masks = torch.stack(masks, dim=0).unsqueeze(1)
+    return masks, scores
+
+    
